4be379dd049dafc2e3bc7936236bc4acf46cadcb
Mark Ellis <ellismarkf@gmail.com>
2017-06-07 22:50:25 -0500

    👷 - configure API infrastructure

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.get('/', (req, res) => {
   res.send(' 💣 ');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('Minesweeper API listening on port', process.env.PORT);
+app.get('/minefields', (req, res) => {
+  res.send('GET ALL MINEFIELDS');
+});
+
+app.get('/minefields/:minefieldId', (req, res) => {
+  res.send(req.params);
+});
+
+app.post('/minefields', (req, res) => {
+  res.send('POST NEW MINEFIELD');
+});
+
+app.listen(PORT, () => {
+  console.log('Minesweeper API listening on port', PORT);
 });
